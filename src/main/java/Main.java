b8c0d415978c1649211484c7367315fa9a81a65c
Anhelor <2238040405lex@gmail.com>
2021-07-12 07:55:33 +0800

    Simpilfy Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,11 @@
-import java.io.Console;
-import java.util.Scanner;
-
 import Staff.Employee;
 import Tools.Account;
 import Tools.Menu;
-import Tools.StaffFactory;
-import Tools.Enums.Position;
 
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        Employee user = Login();
+        Employee user = Account.Login();
         menu.Start(user);
-    }
-
-    public static Employee Login() {
-        Scanner input = new Scanner(System.in);
-        Console console = System.console();
-        System.out.println("Welcome to Headhunter Management System");
-        System.out.print("Account: ");
-        String input_account = input.nextLine();
-        System.out.print("Password: ");
-        String input_password = new String(console.readPassword());
-
-        Position position = Account.Verify(input_account, input_password);
-        StaffFactory staff_factory = new StaffFactory();
-        Employee user = null;
-        switch (position) {
-            case EMPLOYEE -> {
-                user = staff_factory.GetEmployee(input_account);
-            }
-            case LEADER -> {
-                user = staff_factory.GetLeader(input_account);
-            }
-            case SUPERUSER -> {
-                user = staff_factory.GetSuperuser(input_account);
-            }
-        }
-
-        return user;
     }
 }
--- src/main/java/Tools/Account.java
+++ src/main/java/Tools/Account.java
@@ -1,10 +1,13 @@
 package Tools;
 
+import java.io.Console;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
+import Staff.Employee;
 import Tools.Enums.Position;
 
 public class Account {
@@ -43,5 +46,32 @@ public class Account {
             e.printStackTrace();
         }
         return position;
+    }
+
+    public static Employee Login() {
+        Scanner input = new Scanner(System.in);
+        Console console = System.console();
+        System.out.println("Welcome to Headhunter Management System");
+        System.out.print("Account: ");
+        String input_account = input.nextLine();
+        System.out.print("Password: ");
+        String input_password = new String(console.readPassword());
+
+        Position position = Account.Verify(input_account, input_password);
+        StaffFactory staff_factory = new StaffFactory();
+        Employee user = null;
+        switch (position) {
+            case EMPLOYEE -> {
+                user = staff_factory.GetEmployee(input_account);
+            }
+            case LEADER -> {
+                user = staff_factory.GetLeader(input_account);
+            }
+            case SUPERUSER -> {
+                user = staff_factory.GetSuperuser(input_account);
+            }
+        }
+
+        return user;
     }
 }
